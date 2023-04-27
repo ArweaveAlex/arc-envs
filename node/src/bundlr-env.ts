a352a4c725a319f304609b9380a9cd5f81ad1f87
NickJ202 <nickjuliano20@gmail.com>
2023-04-27 10:59:27 -0400

    update bundlr-env test

--- node/src/bundlr-env.ts
+++ node/src/bundlr-env.ts
@@ -6,11 +6,14 @@ import * as ArcFramework from 'arcframework';
 (async function () {
     const jwk = JSON.parse(Buffer.from(fs.readFileSync('./wallets/wallet.json')).toString('utf-8'));
 
-    /* Uncomment bundlr instantiation and GQL will not run */
-    // const bundlr = new Bundlr('https://node2.bundlr.network', 'arweave', jwk);
+    /* Uncomment bundlr instantiation and GQL will not run with Alex. Pool Id value */
+    const bundlr = new Bundlr('https://node2.bundlr.network', 'arweave', jwk);
     
+    // Both log 200 response from Arweave
+    // Works - A4wHgj_FqirSt6LiVejyoSqC_DSF22mUFqh4lz5XOIg
+    // Fails - zoljIRyzG5hp-R4EZV2q8kFI49OAoy23_B9YJ_yEEws
     const gqlData: ArcFramework.ArtifactResponseType = await ArcFramework.getArtifactsByPool({
-        ids: ['zoljIRyzG5hp-R4EZV2q8kFI49OAoy23_B9YJ_yEEws'],
+        ids: ['A4wHgj_FqirSt6LiVejyoSqC_DSF22mUFqh4lz5XOIg'],
         owner: null,
         uploader: null,
         cursor: null,
